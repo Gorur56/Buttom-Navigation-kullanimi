1def0ad8db8aeb833520e7fcce1dd7e9cf610855
Gorur56 <sevim.gorur01@gmail.com>
2024-03-17 17:09:56 +0300

    Navigation kullanimi

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import com.android.aaptcompiler.parseNavigation
+
+plugins {
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "com.firstapp.bottomnavigationkullanimi"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.firstapp.bottomnavigationkullanimi"
+        minSdk = 19
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
+    implementation("androidx.navigation:navigation-ui:2.4.0")
+}
